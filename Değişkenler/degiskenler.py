d8b248ea2be995da0bcdfbab1548943eb08ff77e
yusufkontoglu <yusufkontoglu@gmail.com>
2021-01-10 13:38:59 +0300

    Initial commit

---
+++ Değişkenler/degiskenler.py
@@ -0,0 +1,66 @@
+# ## giris
+
+isim = 'ferid'
+
+# print('merhaba    ' + isim + '. Günlük antivirus taramanızı yaptık. Merhaba ' + isim + '. Merhaba ' + isim + '.')
+
+# ## Degisken tanimlama kurallari
+
+# türkçe karakter kullanılmıyor
+# oğuz = 14
+oguz = 14
+
+# Ayrı yazılmaz
+# yusuf kontoglu = 18
+yusuf_kontoglu = 18
+yusufKontoglu = 18
+
+# ## Sayilar numbers
+
+# Tam sayilar integers
+yas = 16
+maas = 4000
+
+# Ondalikli sayilar float
+ucret = 13.99
+
+# ## Matematiksel operatorler ( + - * / ** % // )
+
+# ## Yazi gruplari String 
+
+ahmet = 14
+yazi = "ahmet"
+# print(yazi)
+
+yazi2 =           'ahmet "iyiyim" dedi'
+
+# ## boolean true-false
+
+isOnline = True
+isSelected = False
+
+# ## type()
+
+# print( type(maas) )
+
+# ## input()
+
+# name = input('ismin ne')
+# print(name)
+
+# ## birim donusturmeleri
+
+num1 = 12
+num2 = 2.0
+print(type(num1 + num2))
+
+num3 = '12'
+num4 = 2.0
+print(float(num3) + num4)
+
+num5 = 123
+
+print(str(num5))
+print(type(str(num5)))
+
+num6 = str(num5)
